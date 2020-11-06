3717374a629908f4e0f0c4c3608ad67247e1f7d2
mi0706chi <mi0706chi@gmail.com>
2020-11-07 01:45:23 +0900

    Separate how to draw.(depend on x_s < x_e ?)

--- srcs/line.c
+++ srcs/line.c
@@ -5,9 +5,10 @@ void my_make_line(t_img *img, t_line *line, int color)
 {
 	double a; //傾き
 	double b; //切片
-	int i;
-	int x;
-	int y;
+	double i;
+	double x;
+	double y;
+	int index;
 
 	if (line->x_end == line->x_start) //y軸平衡
 	{
@@ -15,8 +16,14 @@ void my_make_line(t_img *img, t_line *line, int color)
 		x = line->x_start;
 		while (y < line->y_end)
 		{
-			img->data[x + (y - 1) * WIDTH] = color;
-			y++;
+			if (0 > x || x > WIDTH || 0 > y || y > HEIGHT)
+			{
+				y += 1;
+				continue;
+			}
+			index = (int)(x + (y - 1) * WIDTH);
+			img->data[index] = color;
+			y += 1;
 		}
 	}
 	else if (line->y_end == line->y_start) //x軸平衡
@@ -25,8 +32,14 @@ void my_make_line(t_img *img, t_line *line, int color)
 		y = line->y_start;
 		while (x < line->x_end)
 		{
-			img->data[x + (y - 1) * WIDTH] = color;
-			x++;
+			if (0 > x || x > WIDTH || 0 > y || y > HEIGHT)
+			{
+				x += 1;
+				continue;
+			}
+			index = (int)(x + (y - 1) * WIDTH);
+			img->data[index] = color;
+			x += 1;
 		}
 	}
 	else
@@ -34,17 +47,60 @@ void my_make_line(t_img *img, t_line *line, int color)
 		a = ((line->y_end - line->y_start) / (line->x_end - line->x_start));
 		b = line->y_start - (a * line->x_start);
 		i = 0;
-		while ((int)line->x_start + i <= (int)line->x_end)
+		if (line->x_start < line->x_end)
 		{
-			x = (int)(line->x_start + (double)i);
-			y = (int)(a * (line->x_start + (double)i) + b);
-			if ((x + (y - 1) * WIDTH) < 0)
+
+			while ((int)line->x_start + i <= (int)line->x_end)
 			{
-				printf("Error : Array index is minus\n");
-				return;
+				x = (int)(line->x_start + (double)i);
+				if (0 > x || x > WIDTH)
+				{
+					i+=1;
+					continue;
+				}
+				y = (int)(a * x + b);
+				if (0 > y || y > HEIGHT)
+				{
+					i+=1;
+					continue;
+				}
+				index = x + (y - 1) * WIDTH;
+				if (index < 0 || WIDTH * HEIGHT <= index)
+				{
+					i++;
+					printf("Error : Array index is out of definition.\n");
+					continue;
+				}
+				img->data[index] = color; //0 + (0 -1)*WIDTHにアクセスしているからだめ
+				i+=1;
 			}
-			img->data[x + (y - 1) * WIDTH] = color; //0 + (0 -1)*WIDTHにアクセスしているからだめ
-			i++;
+		}
+		else
+		{
+			while ((int)line->x_end <= (int)line->x_start - i)
+			{
+				x = (int)(line->x_start - (double)i);
+				if (0 > x || x > WIDTH)
+				{
+					i+=1;
+					continue;
+				}
+				y = (int)(a * x + b);
+				if (0 > y || y > HEIGHT)
+				{
+					i+=1;
+					continue;
+				}
+				index = x + (y - 1) * WIDTH;
+				if (index < 0 || WIDTH * HEIGHT <= index)
+				{
+					i+=1;
+					printf("Error : Array index is out of definition.\n");
+					continue;
+				}
+				img->data[index] = color; //0 + (0 -1)*WIDTHにアクセスしているからだめ
+				i+=1;
+			}
 		}
 	}
 }
