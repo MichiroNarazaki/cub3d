8a586d0431c1b8df43c81b9c32bbac0101f2b25f
mi0706chi <mi0706chi@gmail.com>
2020-11-07 01:55:11 +0900

    Change mlx_pixel_put to my_make_line

--- srcs/rec.c
+++ srcs/rec.c
@@ -2,45 +2,21 @@
 #include "../includes/main.h"
 
 //原点O(x,y)
-void my_rec_put(t_game *game, int x, int y, int color)
+void my_rec_put(t_img *img, int x, int y, int color)
 {
-	int x_start = x - (SIDE / 2);
-	int y_start = y - (SIDE / 2);
-	int x_end = x + (SIDE / 2); //10が四角の大きさを決定してる(辺の長さ)
-	int y_end = y + (SIDE / 2); //10が四角の大きさを決定してる(辺の長さ)
-	int y_tmp = y_start;
+	t_line line;
+	int i;
+	int x_init;
 
-	while (x_start <= x_end)
+	i = 0;
+	x_init = x - (SIDE / 2);
+	line.y_start = y - (SIDE / 2);
+	line.y_end = y + (SIDE / 2);
+	while (line.x_start < x + (SIDE / 2))
 	{
-		y_start = y_tmp;
-		while (y_start <= y_end)
-		{
-			mlx_pixel_put(game->mlx, game->win, x_start, y_start, color);
-			y_start++;
-		}
-		x_start++;
+		line.x_start = x_init + i;
+		line.x_end = line.x_start;
+		my_make_line(img, &line, color);
+		i++;
 	}
 }
-
-// void my_rec_put(t_img *img, int color)
-// {
-// 	double x = ORIGIN_X;
-// 	double y = ORIGIN_Y;
-// 	double x_start = x - (SIDE / 2);
-// 	double y_start = y - (SIDE / 2);
-// 	double x_end = x + (SIDE / 2); //10が四角の大きさを決定してる(辺の長さ)
-// 	double y_end = y + (SIDE / 2); //10が四角の大きさを決定してる(辺の長さ)
-// 	int i;
-// 	t_line line;
-
-// 	line.y_end = y_end;
-// 	line.y_start = y_start;
-// 	i = 0;
-// 	while (x_start + (double)i <= x_end)
-// 	{
-// 		line.x_start = x_start + (double)i;
-// 		line.x_end = x_start + (double)i;
-// 		my_make_line(img, &line, color);
-// 		i++;
-// 	}
-// }
